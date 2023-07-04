49557d0c1fc19a5152398269547314313554948c
DeMineArchiver <minearchiver@gmail.com>
2023-07-04 16:25:11 +0300

    animate11: basic theme detection

--- animate11/src/main.cc
+++ animate11/src/main.cc
@@ -1,7 +1,14 @@
+#include <animate/jsapi.h>
 #include <Windows.h>
 #include <dwmapi.h>
+#include <filesystem>
+#include <pugixml.hpp>
+#include <regex>
 #include <string>
-#include <animate/jsapi.h>
+#include <cstring>
+#include <map>
+
+namespace fs = std::filesystem;
 
 std::wstring GetWindowClassName(HWND hWnd) {
     wchar_t class_name[MAX_PATH + 1];
@@ -12,6 +19,14 @@ std::wstring GetWindowClassName(HWND hWnd) {
 HRESULT SetWindowCaptionColor(HWND hWnd, COLORREF color) {
     return DwmSetWindowAttribute(hWnd, DWMWA_CAPTION_COLOR, &color, sizeof(COLORREF));
 }
+
+std::map<int, std::pair<COLORREF, COLORREF>> colors = {
+    { 0, { 0x1f1f1f, 0x1b1b1b }},
+    { 25, { 0x333333, 0x1b1b1b }},
+    { 75, { 0xf5f5f5, 0xfdfdfd }},
+    { 100, { 0xffffff, 0xfdfdfd }}
+};
+int theme_brightness = 25;
 
 HHOOK hook;
 LRESULT WINAPI CallWndRetProc(int code, WPARAM wParam, LPARAM lParam) {
@@ -26,9 +41,9 @@ LRESULT WINAPI CallWndRetProc(int code, WPARAM wParam, LPARAM lParam) {
             if((style & WS_CAPTION) == WS_CAPTION || (style & WS_DLGFRAME) == WS_DLGFRAME) {
                 std::wstring class_name = GetWindowClassName(info->hwnd);
                 if(class_name == L"PLUGPLUG_UI_NATIVE_WINDOW_CLASS_NAME") {
-                    SetWindowCaptionColor(info->hwnd, 0x1b1b1b);
+                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].second);
                 } else {
-                    SetWindowCaptionColor(info->hwnd, 0x333333);
+                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].first);
                 }
             }
             break;
@@ -38,15 +53,43 @@ LRESULT WINAPI CallWndRetProc(int code, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(hook, code, wParam, lParam);
 }
 
-// std::map<int, std::pair<COLORREF, COLORREF>> colors = {
-//     { 0, { 0x1f1f1f, 0x1b1b1b }},
-//     { 25, { 0x333333, 0x1b1b1b }},
-//     { 75, { 0xf5f5f5, 0xfdfdfd }},
-//     { 100, { 0xffffff, 0xfdfdfd }}
-// };
+void LoadTheme() {
+    HMODULE handle;
+    GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCWSTR) &LoadTheme, &handle);
+    wchar_t file_name[MAX_PATH + 1];
+    GetModuleFileNameW(handle, file_name, MAX_PATH + 1);
+
+    fs::path path(file_name, fs::path::generic_format);
+    std::wregex regex(L"^(.:\\/Users\\/.+?\\/AppData)\\/Local\\/Adobe\\/Animate (\\d+)\\/(.+?)\\/Configuration\\/External Libraries");
+    std::wsmatch matches;
+
+    std::wstring path_string = path.generic_wstring();
+
+    if(std::regex_search(path_string, matches, regex)) {
+        std::wstring appdata = matches[1].str();
+        std::wstring version = matches[2].str();
+        std::wstring locale = matches[3].str();
+        
+
+        fs::path roaming = appdata;
+        roaming /= "Roaming/Adobe/Animate";
+        roaming /= version;
+        roaming /= "Application.xml";
+
+        pugi::xml_document document;
+        if(document.load_file(roaming.generic_wstring().c_str())) {
+            for(auto& child : document.child("prop.map").child("prop.list").children()) {
+                if(!child.child("key") || !child.child("float")) continue;
+                if(std::strcmp(child.child("key").text().as_string(), "ThemeBrightness") == 0) theme_brightness = child.child("float").text().as_float() * 100;
+            }
+            MessageBoxW(nullptr, std::to_wstring(theme_brightness).c_str(), L"bright", MB_OK | MB_ICONEXCLAMATION);
+        }
+    }
+}
 
 ANIMATE_INIT(Init)
 void Init() {
+    LoadTheme();
     hook = SetWindowsHookExW(WH_CALLWNDPROCRET, &CallWndRetProc, nullptr, GetCurrentThreadId());
 }
 
