0a7e7cbf1ab3b0dae962a188270eb5ec4023b0a6
DeMineArchiver <minearchiver@gmail.com>
2023-07-22 12:16:40 +0300

    animate11: small fixes

--- animate11/src/main.cc
+++ animate11/src/main.cc
@@ -2,25 +2,30 @@
 #include <Windows.h>
 #include <dwmapi.h>
 #include <filesystem>
+#include <libloaderapi.h>
 #include <pugixml.hpp>
 #include <regex>
 #include <string>
 #include <cstring>
-#include <map>
+#include <unordered_map>
 
 namespace fs = std::filesystem;
-
-
 
 typedef HRESULT(WINAPI *pDwmSetWindowAttribute)(HWND hWnd, DWORD dwAttribute, LPCVOID pvAttribute, DWORD cbAttribute);
 typedef HRESULT(WINAPI *pDwmExtendFrameIntoClientArea)(HWND hWnd, const MARGINS* pMarInset);
 
-typedef VOID(WINAPI *pRtlGetNtVersionNumbers)(DWORD* majorVersion, DWORD* minorVersion, DWORD* buildNumber);
+typedef void(WINAPI *pRtlGetNtVersionNumbers)(DWORD* majorVersion, DWORD* minorVersion, DWORD* buildNumber);
+
+void RtlGetNtVersionNumbers(unsigned long* major_ptr, unsigned long* minor_ptr, unsigned long* build_ptr) {
+    static HMODULE ntdll = nullptr;
+    static pRtlGetNtVersionNumbers rtl_get_nt_version_numbers = nullptr;
+    if(!ntdll) ntdll = GetModuleHandleW(L"ntdll.dll");
+    if(!rtl_get_nt_version_numbers) rtl_get_nt_version_numbers = reinterpret_cast<pRtlGetNtVersionNumbers>(GetProcAddress(ntdll, "RtlGetNtVersionNumbers"));
+    return rtl_get_nt_version_numbers(major_ptr, minor_ptr, build_ptr);
+}
 
 bool enabled = false;
 void TryEnable() {
-    const auto RtlGetNtVersionNumbers = 
-        reinterpret_cast<pRtlGetNtVersionNumbers>(GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlGetNtVersionNumbers"));
     DWORD major, build_number;
     RtlGetNtVersionNumbers(&major, nullptr, &build_number); 
 
@@ -42,21 +47,26 @@ std::wstring GetWindowClassName(HWND hWnd) {
     GetClassNameW(hWnd, class_name, MAX_PATH + 1);
     return std::wstring(class_name);
 }
-bool SetWindowCaptionColor(HWND hWnd, COLORREF color) {
+bool SetWindowCaptionColor(HWND hWnd, unsigned long color) {
     auto& dwmapi = GetDWMAPI();
     if(!enabled || !dwmapi) return false;
     const auto DwmSetWindowAttribute = reinterpret_cast<pDwmSetWindowAttribute>(GetProcAddress(dwmapi, "DwmSetWindowAttribute"));
     return DwmSetWindowAttribute ? DwmSetWindowAttribute(hWnd, DWMWA_CAPTION_COLOR, &color, sizeof(COLORREF)) == S_OK : false;
 }
 
-std::map<int, std::pair<COLORREF, COLORREF>> colors = {
-    { 0, { 0x1f1f1f, 0x1b1b1b }},
-    { 25, { 0x333333, 0x1b1b1b }},
-    { 75, { 0xf5f5f5, 0xfdfdfd }},
-    { 100, { 0xffffff, 0xfdfdfd }}
+struct AnimateColors {
+    unsigned long primary;
+    unsigned long secondary;
 };
 
-int theme_brightness = 25;
+std::unordered_map<float, AnimateColors> colors = {
+    { 0, { 0x1f1f1f, 0x1b1b1b } },
+    { 0.25, { 0x333333, 0x1b1b1b } },
+    { 0.75, { 0xf5f5f5, 0xfdfdfd } },
+    { 1, { 0xffffff, 0xfdfdfd } }
+};
+
+float theme_brightness = 0.25;
 
 HHOOK hook;
 LRESULT WINAPI CallWndRetProc(int code, WPARAM wParam, LPARAM lParam) {
@@ -71,9 +81,9 @@ LRESULT WINAPI CallWndRetProc(int code, WPARAM wParam, LPARAM lParam) {
             if((style & WS_CAPTION) == WS_CAPTION || (style & WS_DLGFRAME) == WS_DLGFRAME) {
                 std::wstring class_name = GetWindowClassName(info->hwnd);
                 if(class_name == L"PLUGPLUG_UI_NATIVE_WINDOW_CLASS_NAME") {
-                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].second);
+                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].secondary);
                 } else {
-                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].first);
+                    SetWindowCaptionColor(info->hwnd, colors[theme_brightness].primary);
                 }
             }
             break;
@@ -116,7 +126,8 @@ bool LoadTheme() {
     for(auto& child : document.child("prop.map").child("prop.list").children()) {
         if(!child.child("key") || !child.child("float")) continue;
         if(std::strcmp(child.child("key").text().as_string(), "ThemeBrightness") == 0) {
-            theme_brightness = child.child("float").text().as_float() * 100;
+            theme_brightness = child.child("float").text().as_float();
+            if(!colors.contains(theme_brightness)) theme_brightness = 0.25;
             return true;
         }
     }
